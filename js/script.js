dea986325a3c5d22fee5df0d56e00e5e4ec4ddae
Erik <gaming23@mein.gmx>
2025-01-04 15:22:46 +0100

    update js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const mainContent = document.getElementById('main-content');
 const enterButton = document.getElementById('enter-button');
 const volumeControl = document.getElementById('volume-control');
 const volumeSlider = document.getElementById('volume-slider');
+const volumeIcon = document.querySelector('.volume-icon'); // Icon-Element
 const backgroundMusic = document.getElementById('background-music');
 
 // Initial volume
@@ -29,4 +30,22 @@ volumeControl.addEventListener('mouseleave', () => {
 volumeSlider.addEventListener('input', (e) => {
     const value = parseFloat(e.target.value);
     backgroundMusic.volume = value;
+
+    // Update the icon based on volume level
+    if (value === 0) {
+        volumeIcon.src = 'assets/volumen-muted.png'; // Mute Icon
+    } else {
+        volumeIcon.src = 'assets/volume-low-white-icon.png'; // Normal Icon
+    }
+});
+
+// Toggle mute/unmute on icon click
+volumeIcon.addEventListener('click', () => {
+    if (backgroundMusic.volume === 0) {
+        backgroundMusic.volume = 0.5;  // Set to previous volume (can be any value)
+        volumeIcon.src = 'assets/volume-low-white-icon.png'; // Normal Icon
+    } else {
+        backgroundMusic.volume = 0;  // Mute
+        volumeIcon.src = 'assets/volumen-muted.png'; // Mute Icon
+    }
 });
